c940751fd19adc37000903c3c302a0f1d39bfe43
Mario Velandia <mariostyvenv@gmail.com>
2020-04-25 15:43:56 -0500

    Se resuelve el error del componente que no refresca

--- client/src/componentes/Clientes.js
+++ client/src/componentes/Clientes.js
@@ -1,63 +1,57 @@
 import React, {Fragment, useEffect, useState} from 'react'
-import {Query, Mutation} from 'react-apollo'
+import {useQuery, useMutation} from 'react-apollo'
 import {CLIENTES_QUERY} from '../queries'
 import {ELIMINAR_CLIENTE} from '../mutations'
-import { Link, Redirect} from 'react-router-dom'
+import { Link} from 'react-router-dom'
 
-const Contactos = (props) => {
+const Contactos = () => {    
+    const { data, error, loading } = useQuery(CLIENTES_QUERY,{
+        fetchPolicy: 'network-only'
+    })
 
-    const [list, setList] = useState([])
-
-    return(
-        <Query query={CLIENTES_QUERY} fetchPolicy='network-only'>
-            {({loading, error, data}) => {
-
-                if(loading) return "Cargando..."
-                if(error) return `Error: ${error.message}`
+    if(loading) return "Cargando..."
+    if(error) return `Error: ${error.message}`
                 
-                return(
-                    <Prueba data={data} loading={loading} error={error}/> 
-                )
-            }}
-        </Query>
-    )
+    return <Prueba data={data}/> 
 }
-const Prueba = ({data, loading, error}) => {
+
+const Prueba = ({data}) => {
+    const [list, setList] = useState([])
+    const [eliminarCliente] = useMutation(ELIMINAR_CLIENTE)
 
     useEffect(() => {
-        
-    },[data])
+        setList([...data.getClientes])
+    }, [data])
+
+    const deleteRegistro = indice => {
+        list.splice(indice, 1);
+        setList([...list])
+    }
 
     return (
         <Fragment>
             <h2 className="text-center">Listado Clientes</h2>
             <ul className="list-group mt-4">
-                {data.getClientes.map(item => (
+                {list.map((item, i) => (
                     <li key={item.id} className="list-group-item">
                         <div className="row justify-content-between align-items-center">
                             <div className="col-md-8 d-flex justify-content-between align-items-center">
                                 {item.nombre} {item.apellido} - {item.empresa}
                             </div>
                             <div className="col-md-4 d-flex justify-content-end">
-                                <Mutation 
-                                    mutation={ELIMINAR_CLIENTE}
-                                    onCompleted={alert('df')}
+                               <button 
+                                    type="button" 
+                                    className="btn btn-danger d-block d-md-inline-block mr-2"
+                                    onClick={()=> {
+                                        eliminarCliente({
+                                            variables: {id:item.id}
+                                        })
+                                        deleteRegistro(i)
+                                    }}
                                 >
-                                    {eliminarCliente => (
-                                        <button 
-                                            type="button" 
-                                            className="btn btn-danger d-block d-md-inline-block mr-2"
-                                            onClick={()=>{
-                                                eliminarCliente({
-                                                    variables: {id:item.id}
-                                                })
-                                            }}
-
-                                        >
-                                            &times; Eliminar
-                                        </button>
-                                    )}
-                                </Mutation>
+                                    &times; Eliminar
+                                </button>
+                               
                                 <Link to={`/cliente/editar/${item.id}`} className="btn btn-success d-block d-md-inline-block">
                                     Editar cliente
                                 </Link>
